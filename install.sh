972eb84a4499ac07a3fb9ab403933fe65d3b5e1f
Gustavo <gustavo.oliveira3@academico.ufpb.br>
2026-01-14 03:11:38 -0300

    parameter and instalation paths fixing

--- install.sh
+++ install.sh
@@ -256,6 +256,57 @@ make -j$(nproc)
 
 sudo make install
 
+cd $BASE_DIR
+
+#=========================================
+#               LIVOX
+#=========================================
+
+echo "LIVOX"
+
+rm -rf Livox-SDK2
+
+git clone git@github.com:Livox-SDK/Livox-SDK2.git
+cd Livox-SDK2
+
+sed -i '1i #include <cstdint>' sdk_core/comm/define.h
+sed -i '1i #include <cstdint>' sdk_core/logger_handler/file_manager.h
+sed -i '1i #include <cstdint>' sdk_core/comm/comm_port.h
+sed -i '1i #include <cstdint>' sdk_core/params_check.h
+sed -i '1i #include <cstdint>' sdk_core/comm/sdk_protocol.h
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
+
+sudo make install
+
+sudo ldconfig
+
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib' >> ~/.bashrc
+
+cd $BASE_DIR
+
+#=========================================
+#               ACADOS
+#=========================================
+
+rm -rf acados 
+
+git clone https://github.com/acados/acados.git
+cd acados
+
+git submodule update --recursive --init
+
+mkdir -p build
+cd build
+
+cmake .. -DACADOS_WITH_QPOASES=ON -DCMAKE_INSTALL_PREFIX=..
+
+make -j$(nproc)
+make install
+
 cd $THIS_DIR
 
 #==========================================
@@ -269,7 +320,9 @@ rm -rf build
 mkdir -p build
 cd build
 
-cmake .. -DCMAKE_INSTALL_PREFIX=~/gazebo_install -DCMAKE_BUILD_TYPE=Release
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
+
+cmake .. -DCMAKE_INSTALL_PREFIX=~/gazebo_install -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/usr/local -DPKG_CONFIG_PATH=/usr/local/lib/pkgconfig
 
 make -j$(nproc)
 
@@ -277,20 +330,37 @@ make install
 
 sudo ldconfig
 
+sudo apt update
+
+sudo apt install -y ros-jazzy-tinyxml-vendor ros-jazzy-tinyxml2-vendor
+
+
 #===========================================
 #               RUNTIME_ENV_CONFIG
 #===========================================
 
-cat << EOF > ~/gazebo_env.sh
+cat << 'EOF' > ~/gazebo_env.sh
 
 #!/bin/bash
 
 # 1. Configurações do Gazebo Local
-export GAZEBO_PREFIX=~/gazebo_install
-export PATH=$GAZEBO_PREFIX/bin:$PATH
-export LD_LIBRARY_PATH=$GAZEBO_PREFIX/lib:$LD_LIBRARY_PATH
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PREFIX/lib/gazebo-11/plugins
-export GAZEBO_RESOURCE_PATH=$GAZEBO_PREFIX/share/gazebo-11
+export GAZEBO_PREFIX="$HOME/gazebo_install"
+export PATH="$GAZEBO_PREFIX/bin:$PATH"
+
+export LD_LIBRARY_PATH="$GAZEBO_PREFIX/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
+
+export GAZEBO_PLUGIN_PATH="$GAZEBO_PREFIX/lib/gazebo-11/plugins"
+export GAZEBO_RESOURCE_PATH="$GAZEBO_PREFIX/share/gazebo-11"
+
+if [ -d "$GAZEBO_PREFIX/lib/cmake/gazebo-11" ]; then
+    export gazebo_DIR="$GAZEBO_PREFIX/lib/cmake/gazebo-11"
+else
+    export gazebo_DIR="$GAZEBO_PREFIX/lib/cmake/gazebo"
+fi
+
+export CMAKE_PREFIX_PATH="$GAZEBO_PREFIX:$CMAKE_PREFIX_PATH"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$GAZEBO_PREFIX/lib/pkgconfig"
 
 # 2. Configurações do ROS e Workspace
 source /opt/ros/jazzy/setup.bash
@@ -298,7 +368,7 @@ source ~/laser_uav_system_ws/install/setup.bash
 
 # 3. Adiciona os Recursos do Projeto (Drones, Mundos)
 export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:~/laser_uav_system_ws/install/laser_gazebo_resources/share/laser_gazebo_resources
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:~/laser_uav_system_ws/build/gazebo_plugins
+export GAZEBO_PLUGIN_PATH="$GAZEBO_PLUGIN_PATH:$HOME/laser_uav_system_ws/install/gazebo_plugins/lib"
 
 # 4. Fix Gráfico
 export QT_QPA_PLATFORM=xcb
@@ -306,6 +376,13 @@ export QT_QPA_PLATFORM=xcb
 EOF
 
 chmod +x ~/gazebo_env.sh
+
+if ! grep -q "source ~/gazebo_env.sh" ~/.bashrc; then
+    echo 'source ~/gazebo_env.sh' >> ~/.bashrc
+    echo "Adicionado ao .bashrc"
+fi
+
+source ~/.bashrc
 
 echo "Finished"
 
